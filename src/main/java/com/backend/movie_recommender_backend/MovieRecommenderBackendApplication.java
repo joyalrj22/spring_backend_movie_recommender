447b4c7430eb908cf33a6965c38aea254d212228
Joyal Raju <joyalrj22@gmail.com>
2023-01-25 21:29:06 +0000

    init spring web

---
+++ src/main/java/com/backend/movie_recommender_backend/MovieRecommenderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.movie_recommender_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/movie_recommender_backend/MovieRecommenderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.movie_recommender_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
